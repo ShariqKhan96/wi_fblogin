9c42951aaa2e3ba3d4cc2a4aee636950fae97016
shariq <shariqmack@gmail.com>
2018-10-18 02:52:28 -0700

    initial

---
+++ app/src/main/java/android/webinnovatives/com/fblogin/MainActivity.java
@@ -0,0 +1,39 @@
+package android.webinnovatives.com.fblogin;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            PackageInfo info =getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i("ShaKey", "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("Exception", e.getMessage());
+        } catch (Exception e) {
+            Log.e("Exception", e.getMessage());
+        }
+
+    }
+
+
+}
